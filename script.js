e476b3bb1858cf880fe024b49a8fc75fd3b7a74b
yan-3121 <ty.ty.ty.ty.0004@gmail.com>
2023-02-10 12:57:50 +0900

    add:first-commit
    fix

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.hero-wrapper').vegas({
+    slides: [
+        { src: 'wadanouen-slide/assets/img/mainvisual@2x.jpg'},
+        { src: 'wadanouen-slide/assets/img/mainvisual2.jpg'},
+        { src: 'wadanouen-slide/assets/img/mainvisual3.jpg'},
+    ],
+    transition: 'fade'
+});
