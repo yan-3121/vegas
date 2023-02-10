31a73da5c87e3bdde3b3e76a5d3a863c035a6e39
yan-3121 <ty.ty.ty.ty.0004@gmail.com>
2023-02-10 12:57:29 +0900

    first-commit

---
+++ function.js
@@ -0,0 +1,9 @@
+$("#slideshow").vegas({
+    slides: [
+      { src: "./mainvisual@2x.jpg" },
+      { src: "./mainvisual2.jpg" },
+      { src: "./mainvisual3.jpg" }
+    ],
+    animation: 'kenburns',
+    animationDuration: 10000
+  });
